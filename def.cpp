b5654a011aa5a50404205a29af3309cde15ed322
Sanjay <sanjaykannu27@gmail.com>
2020-04-02 04:15:02 +0530

    parametrised

--- def.cpp
+++ def.cpp
@@ -1,15 +1,16 @@
-#include <template/template_ros.hpp>
+#include <template/trajectory_headers.hpp>
 
 namespace ariitk::trajectory_gen{
     
 void trajectory::init(ros::NodeHandle& nh){
     trajectory_pub=nh.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel",10);
+    nh.getParam("linear_vel",linear_vel);
+    nh.getParam("ang_vel",ang_vel);
+    msg.angular.z=ang_vel;
+    msg.linear.x=linear_vel;
 }
 
 void trajectory::run(){
-    geometry_msgs::Twist msg;
-    msg.angular.z=0.5;//optimum value
-    msg.linear.x=5;
     trajectory_pub.publish(msg);
 }
 }//namespace ariitk::namespace_name;
