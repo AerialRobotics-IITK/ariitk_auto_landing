1372a6ba70bf8f0f1b64a9818c6d305520a18a9a
Kshitij1K <kabeer@iitk.ac.in>
2020-04-05 20:22:52 +0530

    renamed and edited files

--- auto_landing/include/auto_landing/trajectory_generation_tracker.hpp
+++ auto_landing/include/auto_landing/trajectory_generation_tracker.hpp
@@ -1,10 +1,6 @@
-#include <XmlRpcValue.h>
-
 #include <ros/ros.h>
-
 #include <mav_msgs/eigen_mav_msgs.h>
 #include <mav_msgs/conversions.h>
-
 #include <trajectory_msgs/MultiDOFJointTrajectory.h>
 #include <gazebo_msgs/ModelStates.h>
 
@@ -12,27 +8,37 @@
 #include <mav_trajectory_generation/trajectory_sampling.h>
 #include <mav_trajectory_generation_ros/ros_visualization.h>
 
-namespace ariitk::auto_landing{
-class ExampleTrajectoryGeneration {
+namespace ariitk::auto_landing {
+
+class TrajectoryGenerationTracking {
     public:
-        ExampleTrajectoryGeneration();
-        ~ExampleTrajectoryGeneration(){};
+        TrajectoryGenerationTracking(char** argv);
         void run();
 
     private:
-        int dimension_;
-        int derivative_to_optimize_;
+        void mavOdometryCallback(const nav_msgs::Odometry& msg);
+        void huskyOdometryCallback(const gazebo_msgs::ModelStates& msg);
+        void generateTrajectory (std::vector<mav_trajectory_generation::Vertex> vertices);
+
+        mav_trajectory_generation::Vertex::Vector computePoints();
+
+        bool publish_visualization_;
+
         double v_max_;
         double a_max_;
         double distance_;
 
-        XmlRpc::XmlRpcValue points_;
+        int dimension_;
+        int derivative_to_optimize_;
+
         Eigen::Vector3d husky_acceleration_;
 
         mav_trajectory_generation::Trajectory result_;
 
         trajectory_msgs::MultiDOFJointTrajectory generated_trajectory_;
+
         visualization_msgs::MarkerArray markers_;
+
         nav_msgs::Odometry mav_odom_, husky_odom_;
 
         ros::NodeHandle nh_;
@@ -41,17 +47,6 @@ class ExampleTrajectoryGeneration {
         ros::Publisher trajectory_pub_, marker_pub_;
 
         ros::Subscriber mav_odometry_sub_, husky_odometry_sub_;
-
-        // double current_time_;
-
-        void generateTrajectory (std::vector<mav_trajectory_generation::Vertex> vertices);
-        double getValueAsDouble (XmlRpc::XmlRpcValue& value);
-        int setConstVariables (std::string argument);
-        mav_trajectory_generation::Vertex::Vector generateLinearTrajectory();
-
-        void mavOdometryCallback(const nav_msgs::Odometry& msg);
-        void huskyOdometryCallback(const gazebo_msgs::ModelStates& msg);
-
 };
 
 } //namespace ariitk::auto_landing
