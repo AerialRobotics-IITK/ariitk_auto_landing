92e73050df6941c7e35f3343d87c2b3f82486f18
Sanjay <sanjaykannu27@gmail.com>
2020-04-19 17:50:03 +0530

    making more robust

--- auto_landing/include/auto_landing/detect_platform.hpp
+++ auto_landing/include/auto_landing/detect_platform.hpp
@@ -1,33 +1,42 @@
 #pragma once
 
-#include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.h>
-#include <opencv2/opencv.hpp>
 #include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
 
-namespace ariitk::detect {
-class platform_detect {
-    private:
-		image_transport::Publisher image_pub;
-		image_transport::Publisher image_pub_preprocess;
-		ros::Subscriber image_sub;
+namespace ariitk::detect
+{
+       class platform_detect
+       {
+       private:
+              image_transport::Publisher image_pub;
+              image_transport::Publisher image_pub_preprocess;
+              ros::Subscriber image_sub;
+              ros::Subscriber quad_height_sub;
+              ros::Publisher platform_centre_pub;
 
-		std::vector<double> cam_mat;
-		std::vector<double> dist_coeff;
-		bool is_undistort;
-		bool publish_preprocess;
-		bool publish_detected_platform;
-		int low_threshold_canny;
-		int upper_threshold_canny;
+              std::vector<double> cam_mat;
+              std::vector<double> dist_coeff;
+              bool is_undistort;
+              bool publish_preprocess;
+              bool publish_detected_platform;
+              int low_threshold_canny;
+              int upper_threshold_canny;
+              double quad_height;
+              double error_limit;
+              int contour_perimeter_thresh,contour_perimeter_scale;
+              geometry_msgs::Point center;
 
-    public:
-		void imageCb(const sensor_msgs::ImageConstPtr& msg);
-		void init(ros::NodeHandle& nh, ros::NodeHandle& nh_private);
-		cv::Mat preprocess(cv::Mat& img,
-			std::vector<double>& cam_mat,
-			std::vector<double>& dist_coeff,
-			bool is_undistort);
-};
-} // namespace ariitk::detect
+       public:
+              void imageCb(const sensor_msgs::ImageConstPtr& msg);
+              void heightCb(const nav_msgs::Odometry& height_msg);
+              void init(ros::NodeHandle& nh,ros::NodeHandle& nh_private);
+              void run();
+              cv::Mat preprocess(cv::Mat& img, std::vector<double>& cam_mat,std::vector<double>& dist_coeff, bool is_undistort);
+       };
+}//namespace ariitk::detect
